e36ab624e15d3bbf07be5bb2548cefc79f2b5633
suyuhong <suyuhong@wenxiangcn.com>
2022-10-25 15:46:17 +0800

    正确使用了vite + ts项目中路径别名的用法；了解了为什么ts项目中不能import xxx.ts文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import reactLogo from '@/assets/react.svg'
+import {test} from '@/utils/index'
+import {test as test1} from '@/utils'
+// import viteSvg from '~/vite.svg'
+import  style from './App.module.scss'
 import { Button } from 'antd-mobile'
 
 function App() {
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8888,
+  },
+  resolve: {
+    // 配置别名
+    alias: {
+      '@': path.join(__dirname, './src'),
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: "@import './src/assets/styles/index.scss';"
+      }
+    }
+  },
   plugins: [react()]
 })
